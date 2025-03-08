c9155e2a53fcb254da545920acbb4fd19bee0916
Rhythm <rahejarhythm29@gmail.com>
2025-03-08 14:56:19 +0530

    update app.py

--- app.py
+++ app.py
@@ -31,5 +31,6 @@ from routes import *
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    with app.app_context():
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host="0.0.0.0", port=port)
