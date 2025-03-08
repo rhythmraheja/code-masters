b4ccd6e817ad4e853647bffaa213dd1a0cf58990
Rhythm <rahejarhythm29@gmail.com>
2025-03-08 15:01:17 +0530

    update app.py

--- app.py
+++ app.py
@@ -8,29 +8,30 @@ from config import CurrentConfig
 from models import db, Teacher
 
 app = Flask(__name__, static_folder="static")
-app.config.from_object(CurrentConfig)
+with app.app_context():
+    app.config.from_object(CurrentConfig)
 
 # Load environment variables
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize Extensions
-db.init_app(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager()
-login_manager.login_view = "index"  # Redirect to login if not authenticated
-login_manager.init_app(app)
+    db.init_app(app)
+    bcrypt = Bcrypt(app)
+    login_manager = LoginManager()
+    login_manager.login_view = "index"  # Redirect to login if not authenticated
+    login_manager.init_app(app)
 
 # Import routes AFTER initializing extensions
-from routes import *
+    from routes import *
 
 
 
 # Run database check at startup
 
 
-if __name__ == "__main__":
-    with app.app_context():
+    if __name__ == "__main__":
         port = int(os.environ.get("PORT", 5000))
         app.run(host="0.0.0.0", port=port)
