aa600912c045ef3da9887311fdfc76bf7c7c1368
Rhythm <rahejarhythm29@gmail.com>
2025-03-08 14:35:49 +0530

    update app.py

--- app.py
+++ app.py
@@ -25,26 +25,10 @@ login_manager.init_app(app)
 # Import routes AFTER initializing extensions
 from routes import *
 
-def check_and_insert_teacher():
-    """Check if a teacher exists and insert if not."""
-    name = 'Ashish'
-    email = 'ashish@gmail.com'
-    password = 'abcd1234'
 
-    with app.app_context():
-        existing_teacher = Teacher.query.filter_by(email=email).first()
-        if not existing_teacher:
-            hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-            new_teacher = Teacher(name=name, email=email, password=hashed_password)
-            db.session.add(new_teacher)
-            db.session.commit()
-            print(f"New teacher {name} inserted successfully!")
 
 # Run database check at startup
-@app.before_request
-def startup():
-    with app.app_context():
-        check_and_insert_teacher()
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
